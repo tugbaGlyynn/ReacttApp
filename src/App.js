19fdb1d081d9a0ad0d4c3086e9140a046c142911
tugbaGlyynn <90762153+tugbaGlyynn@users.noreply.github.com>
2021-12-18 19:45:45 +0300

    React

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
+import alertify from "alertifyjs";
+import { Router } from "react-router";
+import { Route, Switch  } from "react-router-dom";
+import NotFound from "./NotFound";
+import CartList from "./CartList";
+
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] };
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({ currentCategory: category.categoryName });
+    //console.log(category);//hataya baktim
+    this.getProducts(category.id);
+  };
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId; //url degisti
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+  addToCart = (product) => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find((c) => c.product.id === product.id); //eleman daha once eklendi mi
+    if (addedItem) {
+      addedItem.quantity += 1;
+    } else {
+      newCart.push({ product: product, quantity: 1 }); //diziye eleman eklendi
+    }
+
+    this.setState({ cart: newCart });
+    alertify.success(product.productName + " Added To Cart!", 2);
+  };
+
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter((c) => c.product.id !== product.id);
+    this.setState({ cart: newCart });
+  };
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
+    return (
+      <div>
+        <Container>
+          <Navi removeFromCart={this.removeFromCart} cart={this.state.cart} />
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
+            </Col>
+
+            <Col xs="9">
+             <Switch>
+               <Route exact path="/" component={NotFound}></Route>
+               <Route exact path="/cart" component={CartList}></Route>
+             </Switch>
+              <ProductList
+                products={this.state.products}
+                addToCart={this.addToCart}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
